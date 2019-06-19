e959cafa12480c0dde396590cfae09fa554eedcb
Vinay Bharadwaj <vnay92@gmail.com>
2019-06-19 15:27:01 +0530

    Added Models Folder and Laravel Passport

--- config/app.php
+++ config/app.php
@@ -165,16 +165,16 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
